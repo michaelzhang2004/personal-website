866169f9b6c6360ac7773e9ef714e1e325fb94b5
michaelzhang2004 <michaelzhang2004@gmail.com>
2025-01-01 16:30:14 -0600

    Refreshed website design

--- script.js
+++ script.js
@@ -5,23 +5,21 @@
 const faders = document.querySelectorAll('.fade-in');
 
 const appearOptions = {
-  threshold: 0.1, // The percentage of the element's visibility at which the observer's callback is executed
+  threshold: 0.1, // The percentage of the element's visibility before callback
 };
 
-const appearOnScroll = new IntersectionObserver((entries, appearOnScroll) => {
-  entries.forEach((entry) => {
-    if (!entry.isIntersecting) {
-      return;
-    } else {
-      // Add the 'appear' class once in view
-      entry.target.classList.add('appear');
-      // Optionally unobserve the element after it has appeared
-      appearOnScroll.unobserve(entry.target);
-    }
+const appearOnScroll = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) return;
+    // Add the 'appear' class once in view
+    entry.target.classList.add('appear');
+    // Unobserve the element after it has appeared
+    observer.unobserve(entry.target);
   });
 }, appearOptions);
 
 // 2. Attach the observer to each .fade-in element
-faders.forEach((fader) => {
+faders.forEach(fader => {
   appearOnScroll.observe(fader);
 });
+
